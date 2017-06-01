bbc6b7ecc2ace7c0dab3a64bbd88595fea741d58
Eduardo <eduard_alfons@hotmail.com>
2017-06-01 11:06:15 -0600

    Agregué la clase conexión, ya corregida, además, arreglé la clase UsuarioLog, no hay nada que revisar por el momento.

---
+++ ACOPEDH/ACOPEDH/Conexión.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ACOPEDH
+{
+    class Conexión
+    {
+        public string servidor = Globales.Servidor;
+        public string usuario = "InicioSesion";
+        public string clave = "In112358";
+        public string db = "ACOPEDH";
+        public string cadena;
+        public Conexión()
+        {
+            cadena = @"Data Source = " + Globales.Servidor + ";Initial Catalog =" + db + ";User=" + usuario + ";Password=" + clave;
+        }
+        public Conexión(string pusuario, string pclave)
+        {
+            this.usuario = pusuario;
+            this.clave = pclave;
+            cadena = @"Data Source = " + Globales.Servidor + ";Initial Catalog =" + db + ";User=" + usuario + ";Password=" + clave;
+        }
+    }
+}
