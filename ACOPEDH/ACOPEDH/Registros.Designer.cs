e8106740e46e595cd1fd5763639ef9953956f4ba
GissReyes <gissellereyes09@gmail.com>
2018-01-22 23:18:00 -0600

    Probando

---
+++ ACOPEDH/ACOPEDH/Registros.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ACOPEDH
+{
+    partial class Registros
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GBAsociado = new System.Windows.Forms.GroupBox();
+            this.txtDUI = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCódigo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAsociado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.GBAsociado.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBAsociado
+            // 
+            this.GBAsociado.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.GBAsociado.BackColor = System.Drawing.Color.Transparent;
+            this.GBAsociado.Controls.Add(this.textBox1);
+            this.GBAsociado.Controls.Add(this.label4);
+            this.GBAsociado.Controls.Add(this.txtDUI);
+            this.GBAsociado.Controls.Add(this.label2);
+            this.GBAsociado.Controls.Add(this.txtCódigo);
+            this.GBAsociado.Controls.Add(this.label3);
+            this.GBAsociado.Controls.Add(this.txtAsociado);
+            this.GBAsociado.Controls.Add(this.label1);
+            this.GBAsociado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GBAsociado.Location = new System.Drawing.Point(0, 49);
+            this.GBAsociado.Name = "GBAsociado";
+            this.GBAsociado.Size = new System.Drawing.Size(877, 85);
+            this.GBAsociado.TabIndex = 2;
+            this.GBAsociado.TabStop = false;
+            this.GBAsociado.Text = "Datos";
+            // 
+            // txtDUI
+            // 
+            this.txtDUI.BackColor = System.Drawing.Color.DarkGray;
+            this.txtDUI.Location = new System.Drawing.Point(139, 48);
+            this.txtDUI.Name = "txtDUI";
+            this.txtDUI.ReadOnly = true;
+            this.txtDUI.Size = new System.Drawing.Size(382, 22);
+            this.txtDUI.TabIndex = 79;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(49, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 78;
+            this.label2.Text = "DUI: ";
+            // 
+            // txtCódigo
+            // 
+            this.txtCódigo.BackColor = System.Drawing.Color.DarkGray;
+            this.txtCódigo.Location = new System.Drawing.Point(717, 14);
+            this.txtCódigo.Name = "txtCódigo";
+            this.txtCódigo.ReadOnly = true;
+            this.txtCódigo.Size = new System.Drawing.Size(135, 22);
+            this.txtCódigo.TabIndex = 77;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(562, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 76;
+            this.label3.Text = "Código Asociación:";
+            // 
+            // txtAsociado
+            // 
+            this.txtAsociado.BackColor = System.Drawing.Color.DarkGray;
+            this.txtAsociado.Location = new System.Drawing.Point(139, 14);
+            this.txtAsociado.Name = "txtAsociado";
+            this.txtAsociado.ReadOnly = true;
+            this.txtAsociado.Size = new System.Drawing.Size(382, 22);
+            this.txtAsociado.TabIndex = 73;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(49, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 34);
+            this.label1.TabIndex = 72;
+            this.label1.Text = "Persona\r\nAsociada:";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 140);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(872, 455);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(32, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(182, 14);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(327, 14);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(98, 21);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.DarkGray;
+            this.textBox1.Location = new System.Drawing.Point(717, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(135, 22);
+            this.textBox1.TabIndex = 81;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(562, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 17);
+            this.label4.TabIndex = 80;
+            this.label4.Text = "Fecha de Asociación: ";
+            // 
+            // Registros
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = global::ACOPEDH.Properties.Resources.Fondo_Lalalala;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(877, 596);
+            this.Controls.Add(this.GBAsociado);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Registros";
+            this.Text = "Registros";
+            this.GBAsociado.ResumeLayout(false);
+            this.GBAsociado.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox GBAsociado;
+        private System.Windows.Forms.TextBox txtDUI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCódigo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAsociado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
---
+++ ACOPEDH/ACOPEDH/Retirar Aportaciones.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ACOPEDH
+{
+    partial class Retirar_Aportaciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Retirar_Aportaciones));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSuma = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BarraTítulo = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCódigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCheque = new System.Windows.Forms.TextBox();
+            this.bttRetirarAportación = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtSuma);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Font = new System.Drawing.Font("Linotte-Light", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(48, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(313, 94);
+            this.groupBox1.TabIndex = 87;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Información";
+            // 
+            // txtSuma
+            // 
+            this.txtSuma.Font = new System.Drawing.Font("Linotte-Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSuma.HideSelection = false;
+            this.txtSuma.Location = new System.Drawing.Point(134, 40);
+            this.txtSuma.Name = "txtSuma";
+            this.txtSuma.ReadOnly = true;
+            this.txtSuma.Size = new System.Drawing.Size(105, 25);
+            this.txtSuma.TabIndex = 77;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Linotte-Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(19, 30);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 36);
+            this.label8.TabIndex = 74;
+            this.label8.Text = "Suma de \r\nAportaciones:";
+            // 
+            // BarraTítulo
+            // 
+            this.BarraTítulo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BarraTítulo.BackColor = System.Drawing.Color.Transparent;
+            this.BarraTítulo.Font = new System.Drawing.Font("Gotham Narrow Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BarraTítulo.Image = ((System.Drawing.Image)(resources.GetObject("BarraTítulo.Image")));
+            this.BarraTítulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraTítulo.Name = "BarraTítulo";
+            this.BarraTítulo.Size = new System.Drawing.Size(417, 30);
+            this.BarraTítulo.TabIndex = 88;
+            this.BarraTítulo.Text = "         ACOPEDH - Retiro de Aportaciones";
+            this.BarraTítulo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(4, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox3.TabIndex = 89;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Linotte-Bold", 12F);
+            this.label3.Location = new System.Drawing.Point(55, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 19);
+            this.label3.TabIndex = 93;
+            this.label3.Text = "Código Asociación:";
+            // 
+            // txtCódigo
+            // 
+            this.txtCódigo.Font = new System.Drawing.Font("Linotte-Bold", 12F);
+            this.txtCódigo.Location = new System.Drawing.Point(204, 55);
+            this.txtCódigo.Name = "txtCódigo";
+            this.txtCódigo.ReadOnly = true;
+            this.txtCódigo.Size = new System.Drawing.Size(135, 27);
+            this.txtCódigo.TabIndex = 94;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Linotte-Bold", 12F);
+            this.label1.Location = new System.Drawing.Point(55, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 19);
+            this.label1.TabIndex = 95;
+            this.label1.Text = "No de Cheque:\r\n";
+            // 
+            // txtCheque
+            // 
+            this.txtCheque.Font = new System.Drawing.Font("Linotte-Bold", 12F);
+            this.txtCheque.Location = new System.Drawing.Point(204, 90);
+            this.txtCheque.MaxLength = 8;
+            this.txtCheque.Name = "txtCheque";
+            this.txtCheque.Size = new System.Drawing.Size(135, 27);
+            this.txtCheque.TabIndex = 96;
+            // 
+            // bttRetirarAportación
+            // 
+            this.bttRetirarAportación.Font = new System.Drawing.Font("Linotte-Bold", 12F);
+            this.bttRetirarAportación.Location = new System.Drawing.Point(141, 252);
+            this.bttRetirarAportación.Name = "bttRetirarAportación";
+            this.bttRetirarAportación.Size = new System.Drawing.Size(112, 50);
+            this.bttRetirarAportación.TabIndex = 97;
+            this.bttRetirarAportación.Text = "Retirar\r\nAportaciones\r\n";
+            this.bttRetirarAportación.UseVisualStyleBackColor = true;
+            this.bttRetirarAportación.Click += new System.EventHandler(this.bttRealizarAportación_Click);
+            // 
+            // Retirar_Aportaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ACOPEDH.Properties.Resources.Fondo_Lalalala;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(417, 326);
+            this.Controls.Add(this.bttRetirarAportación);
+            this.Controls.Add(this.txtCheque);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCódigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.BarraTítulo);
+            this.Controls.Add(this.groupBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Retirar_Aportaciones";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Retirar_Aportaciones";
+            this.Load += new System.EventHandler(this.Retirar_Aportaciones_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSuma;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label BarraTítulo;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCódigo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCheque;
+        private System.Windows.Forms.Button bttRetirarAportación;
+    }
+}
