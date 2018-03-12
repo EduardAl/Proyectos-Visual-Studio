c2c2665a6d0f0264e0b591463d229cb8102cce0d
FatimaGaitan <fgaitan.romero@gmail.com>
2018-03-12 10:30:12 -0600

    Clases modelos

---
+++ ACOPEDH/ACOPEDH/Modelos/modelo_Abonos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACOPEDH.Modelos
+{
+    public class modelo_Abonos
+    {
+        private double monto;
+        private double abonoComision;
+        private DateTime fechaRetiro;
+        private modelo_Retiro Modelo;
+        private double montoRetiro;
+        private DateTime fecha_Retiro;
+        public modelo_Abonos()
+        {
+            monto = 0;
+            AbonoComision = 0;
+            montoRetiro = 0;
+            fechaRetiro = new DateTime();
+            fecha_Retiro = new DateTime();
+            Modelo = new modelo_Retiro();
+        }
+        public modelo_Abonos(double monto, double abono)
+        {
+            this.monto = monto;
+            AbonoComision = abono;
+            fechaRetiro = new DateTime();
+            Modelo = new modelo_Retiro();
+        }
+        public double Monto { get => monto; set => monto = value; }
+        public double AbonoComision { get => abonoComision; set => abonoComision = value; }
+        public DateTime FechaRetiro { get => fechaRetiro; set => fechaRetiro = value; }
+        public modelo_Retiro Modelo1 { get => Modelo; set => Modelo = value; }
+        public DateTime Fecha_Retiro { get => fecha_Retiro; set => fecha_Retiro = value; }
+        public double MontoRetiro { get => montoRetiro; set => montoRetiro = value; }
+    }
+}
---
+++ ACOPEDH/ACOPEDH/Modelos/modelo_Retiro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACOPEDH.Modelos
+{
+    public class modelo_Retiro
+    {
+        private double montoRetiro;
+        private DateTime fecha_retiro;
+
+        public double MontoRetiro { get => montoRetiro; set => montoRetiro = value; }
+        public DateTime Fecha_retiro { get => fecha_retiro; set => fecha_retiro = value; }
+    }
+}
