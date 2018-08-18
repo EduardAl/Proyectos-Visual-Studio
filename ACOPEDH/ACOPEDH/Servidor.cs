db6ce8e13cf2eebb3af38bf27377ce480bbfb5d3
FatimaGaitan <fgaitan.romero@gmail.com>
2018-08-18 11:29:01 -0600

    Cambios

--- ACOPEDH/ACOPEDH/Servidor.cs
+++ ACOPEDH/ACOPEDH/Servidor.cs
@@ -10,42 +10,42 @@ namespace ACOPEDH
     #region esto se puede eliminar si se usa host
     class Servidor
     {
-        //Clase que obtiene el servidor local
-        SqlDataSourceEnumerator servidores;
-        DataTable tablaServidores;
-        List<String> listaServidores = new List<String>();
-        public void server()
-        {
-            try
-            {
-                tablaServidores = new DataTable();
-                servidores = SqlDataSourceEnumerator.Instance;
-                if (tablaServidores.Rows.Count == 0)
-                {
-                    tablaServidores = servidores.GetDataSources();
+//        //Clase que obtiene el servidor local
+//        SqlDataSourceEnumerator servidores;
+//        DataTable tablaServidores;
+//        List<String> listaServidores = new List<String>();
+//        public void server()
+//        {
+//            try
+//            {
+//                tablaServidores = new DataTable();
+//                servidores = SqlDataSourceEnumerator.Instance;
+//                if (tablaServidores.Rows.Count == 0)
+//                {
+//                    tablaServidores = servidores.GetDataSources();
 
-                    foreach (DataRow rowServidor in tablaServidores.Rows)
-                    {
-                        if (String.IsNullOrEmpty(rowServidor["InstanceName"].ToString()))
-                            listaServidores.Add(rowServidor["ServerName"].ToString());
-                        else
-                        {
-                            listaServidores.Add(rowServidor["ServerName"] + "\\" + rowServidor["InstanceName"]);
-                        }
-                    }
-#warning Cambiar los servidores 
-                    Globales.Servidor = listaServidores[0];
-                }
-            }
-            catch (SqlException ex)
-            {
-                MessageBox.Show("Error al conectar con el servidor.\n" + "Número del error: " + ex.Number + "\nCódigo del error: " + ex.ErrorCode + "\nError: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al conectar con el servidor." +"\nError: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
-            }
-        }
+//                    foreach (DataRow rowServidor in tablaServidores.Rows)
+//                    {
+//                        if (String.IsNullOrEmpty(rowServidor["InstanceName"].ToString()))
+//                            listaServidores.Add(rowServidor["ServerName"].ToString());
+//                        else
+//                        {
+//                            listaServidores.Add(rowServidor["ServerName"] + "\\" + rowServidor["InstanceName"]);
+//                        }
+//                    }
+//#warning Cambiar los servidores 
+//                    Globales.Servidor = listaServidores[0];
+//                }
+//            }
+//            catch (SqlException ex)
+//            {
+//                MessageBox.Show("Error al conectar con el servidor.\n" + "Número del error: " + ex.Number + "\nCódigo del error: " + ex.ErrorCode + "\nError: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+//            }
+//            catch (Exception ex)
+//            {
+//                MessageBox.Show("Error al conectar con el servidor." +"\nError: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+//            }
+//        }
     }
     #endregion
 }
