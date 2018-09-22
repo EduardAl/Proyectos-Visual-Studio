9478f87121b01e95e277f444c6540d954a203867
FatimaGaitan <fgaitan.romero@gmail.com>
2018-09-22 17:58:00 -0600

    Detalles de Julie

--- ACOPEDH/ACOPEDH/Datos Asociado.cs
+++ ACOPEDH/ACOPEDH/Datos Asociado.cs
@@ -42,8 +42,10 @@ namespace ACOPEDH
             //Llenado de los combobox
             cbAsociación.DataSource = Cargar.llenar_DataTable("[Cargar Tipo Socio]");
             cbAsociación.DisplayMember = "TipoS";
+            cbAsociación.ValueMember = "ID";
             cbOcupación.DataSource = Cargar.llenar_DataTable("[Cargar Ocupaciones]");
             cbOcupación.DisplayMember = "Trabajo";
+            cbOcupación.ValueMember = "ID";
             //Código Asociado
             lbCódigo.Text = "Código de Asociación: " + Dato;
             CargarDatos();
@@ -79,18 +81,22 @@ namespace ACOPEDH
                             try
                             {
                                 Procedimientos_select modificar = new Procedimientos_select();
-                                SqlParameter[] param = new SqlParameter[10];
+                                SqlParameter[] param = new SqlParameter[3];
+                                //Actualizar Asociado
                                 param[0] = new SqlParameter("@Codigo_Asociado", Dato);
-                                param[1] = new SqlParameter("@FK_Tipo_Socio", cbAsociación.Text);
-                                param[2] = new SqlParameter("@Nombres", txtNombres.Text);
-                                param[3] = new SqlParameter("@Apellidos", txtApellidos.Text);
-                                param[4] = new SqlParameter("@DUI", txtDUI.Text);
-                                param[5] = new SqlParameter("@NIT", txtNIT.Text);
-                                param[6] = new SqlParameter("@Residencia", txtDirección.Text);
-                                param[7] = new SqlParameter("@Fecha_Nacimiento", dtNacimiento.Value);
-                                param[8] = new SqlParameter("@Fecha_Asociación", dtAso.Value);
-                                param[9] = new SqlParameter("@FK_Ocupacion", cbOcupación.Text);
+                                param[1] = new SqlParameter("@FK_Tipo_Socio", cbAsociación.SelectedValue);
+                                param[2] = new SqlParameter("@FK_Ocupación", cbOcupación.SelectedValue);
                                 modificar.llenar_tabla("[Actualizar Asociado]", param);
+                                //Actualizar Persona
+                                param = new SqlParameter[7];
+                                param[0] = new SqlParameter("@Codigo_Persona", aux);
+                                param[1] = new SqlParameter("@Nombres", txtNombres.Text);
+                                param[2] = new SqlParameter("@Apellidos", txtApellidos.Text);
+                                param[3] = new SqlParameter("@DUI", txtDUI.Text);
+                                param[4] = new SqlParameter("@NIT", txtNIT.Text);
+                                param[5] = new SqlParameter("@Residencia", txtDirección.Text);
+                                param[6] = new SqlParameter("@Fecha_Nacimiento", dtNacimiento.Value);
+                                modificar.llenar_tabla("[Actualizar Persona]", param);
                                 Modificar(false);
                                 dr = DialogResult.Yes;
                             }
@@ -207,6 +213,7 @@ namespace ACOPEDH
             dtNacimiento.Enabled = enabled;
             bttModificar.Enabled = !enabled;
             bttAceptar.Enabled=bttCancelar.Enabled = enabled;
+            bttAceptar.Visible = bttCancelar.Visible = enabled;
             errorProvider1.Clear();
         }
         public void CargarDatos()
